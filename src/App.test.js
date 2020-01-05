f50446e0cfc9b7575accdddb060006f70449c9f7
Franz Noel Tanglao <franz@tanglao.org>
2020-01-05 13:49:13 -0800

    #1 test: Add test for toolbar

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should have PangPawn title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/PangPawn/i);
   expect(linkElement).toBeInTheDocument();
 });
