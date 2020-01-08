426fd0072db951bf3a0c4efb8834eccdc5c1006a
Franz Noel Tanglao <franz@tanglao.org>
2020-01-08 08:52:28 -0800

    #1 feat: Add sidebar

--- src/components/MainToolbar.js
+++ src/components/MainToolbar.js
@@ -1,24 +1,134 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import {
-    AppBar,
-    Toolbar,
-    Typography
-} from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Hidden } from '@material-ui/core';
 
 
-export default class MainToolbar extends Component {
-    constructor(props) {
-        super(props);
-    }
+const drawerWidth = 240; // 240
+const useStyles = makeStyles(theme=> ({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+}));
 
-    render() {
-        return (
-            <AppBar position="static">
+
+export default function MainToolbar(props) {
+    const { container } = props;
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = ()=> {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = ()=> {
+        setOpen(false);
+    };
+
+    const drawer = (
+        <div>
+            <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+            {['Members'].map((text, index) => (
+                <ListItem button key={text}>
+                <ListItemText primary={text} />
+                </ListItem>
+            ))}
+            </List>
+        </div>
+    );    
+    
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
-                    <Typography variant="h6">PangPawn</Typography>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerOpen}
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>PangMembers</Typography>
                 </Toolbar>
             </AppBar>
-        );
-    }
+            <nav className={classes.drawer} aria-label="mailbox folders">
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                        open={open}
+                        onClose={handleDrawerClose}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
+        </div>
+    );
 }
